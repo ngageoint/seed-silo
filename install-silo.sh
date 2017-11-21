0b1f193878451da025db38c0841015dbdeac9c36
John Tobe <john@tobe.name>
2017-11-20 22:58:22 -0500

    Making fixes to install script discovered when setting up new ec2 instance.

--- install-silo.sh
+++ install-silo.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-sudo initctl stop silo
 build-silo.sh
 sudo cp silo /bin/
 sudo cp silo.conf /etc/init/
+sudo mkdir /usr/silo
+sudo chmod 755 /usr/silo
+sudo initctl stop silo
 sudo initctl start silo
