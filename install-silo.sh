ca362ce33d8c9b296408ad0390fcd1de9145ccc0
John Tobe <jtobe@ball.com>
2017-12-13 09:37:45 -0500

    Use apache proxy for setting CORS

--- install-silo.sh
+++ install-silo.sh
@@ -4,6 +4,7 @@ sudo initctl stop silo
 ./build-silo.sh
 sudo cp silo /bin/
 sudo cp silo.conf /etc/init/
+sudo cp httpd-silo.conf /etc/httpd/conf.d/
 sudo mkdir -p /usr/silo
 sudo chmod 755 /usr/silo
 sudo initctl start silo
