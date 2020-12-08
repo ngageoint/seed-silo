b5e72bf9777d903e231ff46bd319b32ad43d0aeb
Raul <raul.alvarado@appliedis.com>
2020-12-07 23:59:51 -0500

    fixed token expiration and renewal logic

--- registry/v2/AuthToken.go
+++ registry/v2/AuthToken.go
@@ -2,9 +2,11 @@ package v2
 
 import (
 	"encoding/json"
+	"log"
 
 	"net/http"
 	"net/url"
+
 	// "strconv"
 	"time"
 
@@ -25,6 +27,7 @@ func (registry *V2registry) GetOrCreateToken(org string, urlstring string) (APIT
 
 	apiToken := APIToken{}
 	if !Authtokenmap().Check(org) {
+		log.Printf("Creating new auth token...")
 		//token doesent exist yet
 		req, err := http.NewRequest("GET", urlstring, nil)
 
@@ -65,7 +68,6 @@ func (registry *V2registry) GetOrCreateToken(org string, urlstring string) (APIT
 			}
 
 			paresedURL.RawQuery = q.Encode()
-
 			authRequest, err := http.NewRequest("GET", paresedURL.String(), nil)
 
 			if registry.Username != "" || registry.Password != "" {
@@ -89,6 +91,10 @@ func (registry *V2registry) GetOrCreateToken(org string, urlstring string) (APIT
 			decoder := json.NewDecoder(response.Body)
 			err = decoder.Decode(&apiToken)
 			apiToken.Created = time.Now()
+			log.Printf("API expires in: %v", apiToken.ExpiresIn)
+			if apiToken.ExpiresIn == 0 {
+				apiToken.ExpiresIn = float64(0.5) //0 indicates one time use. lets use the token we just got. and not recreate it right away.
+			}
 			// expstr, err := strconv.Atoi(apiToken.expiresIn)
 			apiToken.Expires = time.Now().Add(time.Second * time.Duration(apiToken.ExpiresIn))
 
@@ -102,7 +108,7 @@ func (registry *V2registry) GetOrCreateToken(org string, urlstring string) (APIT
 			return apiToken, err
 		}
 		if apiToken.IsExpired() {
-			registry.RenewToken(apiToken)
+			apiToken, err = registry.RenewToken(apiToken)
 		}
 		return apiToken, nil
 	}
@@ -111,17 +117,23 @@ func (registry *V2registry) GetOrCreateToken(org string, urlstring string) (APIT
 }
 
 func (token *APIToken) IsExpired() bool {
-	if token.Expires.Sub(token.Created).Seconds() < 0 {
-		return true
-	}
-	return false
+	return !inTimeSpan(token.Created, token.Expires, time.Now())
 }
 
-func (registry *V2registry) RenewToken(token APIToken) error {
-	paresedURL, err := url.Parse(token.realm)
+func inTimeSpan(start, end, check time.Time) bool {
+	return check.After(start) && check.Before(end)
+}
 
+func (registry *V2registry) RenewToken(token APIToken) (APIToken, error) {
+	newToken := APIToken{}
+	newToken.realm = token.realm
+	newToken.scope = token.scope
+	newToken.service = token.service
+
+	paresedURL, err := url.Parse(token.realm)
+	log.Print("renewing token...")
 	if err != nil {
-		return err
+		return token, err
 	}
 
 	q := paresedURL.Query()
@@ -129,7 +141,6 @@ func (registry *V2registry) RenewToken(token APIToken) error {
 	if token.scope != "" {
 		q.Set("scope", token.scope)
 	}
-
 	paresedURL.RawQuery = q.Encode()
 
 	authRequest, err := http.NewRequest("GET", paresedURL.String(), nil)
@@ -141,21 +152,19 @@ func (registry *V2registry) RenewToken(token APIToken) error {
 	client := http.Client{
 		Transport: registry.Client.Transport,
 	}
-
 	response, err := client.Do(authRequest)
 	if err != nil {
-		return err
+		return token, err
 	}
 
 	if response.StatusCode != http.StatusOK {
-		return err
+		return token, err
 	}
 	defer response.Body.Close()
-
 	decoder := json.NewDecoder(response.Body)
-	err = decoder.Decode(&token)
+	err = decoder.Decode(&newToken)
 	token.Created = time.Now()
 	// expstr, err := strconv.Atoi(token.expiresIn)
-	token.Expires = time.Now().Add(time.Second * time.Duration(token.ExpiresIn))
-	return nil
+	token.Expires = time.Now().Add(time.Second * time.Duration(newToken.ExpiresIn))
+	return newToken, nil
 }
